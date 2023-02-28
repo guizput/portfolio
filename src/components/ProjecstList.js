b3e1fbdcc1dbb3a3c87a22105cc6ae1ae5b0390c
Guillaume Duran <guillaumeduran2@gmail.com>
2023-02-28 23:18:04 +0100

    attempt to fix deploy

--- src/components/ProjecstList.js
+++ src/components/ProjecstList.js
@@ -1,7 +1,63 @@
 import Projects from './Projects.js';
-import projects from '../data/projects.js';
 
 const ProjectsList = () => {
+
+    const projects = [
+        {
+            id: 1,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FEdieHomageThumbnail.png%3Falt%3Dmedia%26token%3D72c573b5-6389-425c-b947-de63f0f5b2ef&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'Edie Home Page',
+            desc: 'Made with ParcelJS, SASS and a bit of Javascript for the menu',
+            demoLink: 'https://main--wonderful-kulfi-a14ed7.netlify.app/',
+            codeLink: 'https://github.com/guizput/edie-homepage'
+        },
+        {
+            id: 2,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FCheckoutThumbnail.png%3Falt%3Dmedia%26token%3Dc7ffdbe3-7206-44f2-b1e6-a6b99bf81901&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'Checkout page',
+            desc: 'Crafted with SASS',
+            demoLink: 'https://63dd65161659c4007649451a--glittery-mousse-e7ae65.netlify.app/',
+            codeLink: 'https://github.com/guizput/checkout-page'
+        },
+        {
+            id: 3,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FGalleryThumbnail.png%3Falt%3Dmedia%26token%3D92894792-41d1-4d99-8cbb-e828322c87fd&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'Gallery',
+            desc: 'Used CSS grid',
+            demoLink: 'https://63dd29d39bef3c0a4dd2e1ec--wonderful-licorice-7c91cc.netlify.app/',
+            codeLink: 'https://github.com/guizput/my-gallery'
+        },
+        {
+            id: 4,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FrecipeBlogThumbnail.png%3Falt%3Dmedia%26token%3D2d696d3c-a8cb-4c7c-907b-561ae1144cc9&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'Recipe page',
+            desc: 'Crafted with CSS grid',
+            demoLink: 'https://63d938b25899871e097eef19--glowing-daifuku-ca1459.netlify.app/',
+            codeLink: 'https://github.com/guizput/recipe-page'
+        },
+        {
+            id: 5,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FinteriorConsultantThumbnail.png%3Falt%3Dmedia%26token%3Dfb5f8229-8eb9-4d70-b0d4-fbd2829a0cf0&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'Interior Consultant',
+            desc: 'Crafted with bootstrap',
+            demoLink: 'https://63d17b649814292169b9c710--glittery-haupia-3aa6cf.netlify.app/',
+            codeLink: 'https://github.com/guizput/interior-consultant'
+        },
+        {
+            id: 6,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FmyTeamThumbnail.png%3Falt%3Dmedia%26token%3D3dc1bba0-52b9-440d-8a54-fa31ec7c84d8&w=640&q=75',
+            tags: '#HTML #CSS #responsive',
+            title: 'My Team Page',
+            desc: 'Used Bootstrap to make things faster',
+            demoLink: 'https://63d04aff417998386dbff987--bejewelled-stroopwafel-69ba70.netlify.app/',
+            codeLink: 'https://github.com/guizput/my-team'
+        }
+    ];
 
     return (
         <div className='flex flex-col space-y-8 mt-8'>
--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,7 +1,52 @@
 import Skills from './Skills.js';
-import {frontEnd, design} from '../data/skills.js';
 
 const SkillsList = () => {
+
+    const frontEnd = [
+        {
+            id: 1,
+            name: 'React',
+            rank :'w-3/5'
+        },
+        {
+            id: 2,
+            name: 'Javascript',
+            rank :'w-4/5'
+        },
+        {
+            id: 3,
+            name: 'CSS',
+            rank :'w-4/5'
+        },
+        {
+            id: 4,
+            name: 'Vue',
+            rank :'w-3/5'
+        },
+        {
+            id: 5,
+            name: 'Redux',
+            rank :'w-4/5'
+        },
+        {
+            id: 6,
+            name: 'React Native',
+            rank :'w-full'
+        }
+    ];
+    
+    const design = [
+        {
+            id: 1,
+            name: 'Figma',
+            rank :'w-3/5'
+        },
+        {
+            id: 2,
+            name: 'Photoshop',
+            rank :'w-4/5'
+        }
+    ];
 
     return ( 
         <div className='flex flex-col space-y-8'>
