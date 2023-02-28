810e56473ce070fbaa92ecf765bacd0ce076a00c
Guillaume Duran <guillaumeduran2@gmail.com>
2023-02-28 22:37:41 +0100

    app done

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import ProjectsList from "./components/ProjecstList.js";
 
 const App = () => {
   return (
-    <div className="py-8 px-4 flex flex-col space-y-8">
-      <Profile />
-      <Experience />
-      <Hobbies />
-      <SkillsList />
-      <ProjectsList />
+    <div className="flex flex-col space-y-8 pb-8 px-4 max-w-md mx-auto md:flex-row md:max-w-5xl md:items-start md:space-x-8">
+      <div className="flex flex-col mt-8 md:w-1/3">
+        <Profile />
+        <Experience />
+        <Hobbies />
+      </div>
+      <div className="flex flex-col mt-8 md:w-2/3">
+        <SkillsList />
+        <ProjectsList />
+      </div>
     </div>
   );
 };
--- src/components/ProjecstList.js
+++ src/components/ProjecstList.js
@@ -4,7 +4,7 @@ import projects from '../data/projects.js';
 const ProjectsList = () => {
 
     return (
-        <div className='flex flex-col space-y-8'>
+        <div className='flex flex-col space-y-8 mt-8'>
             <div className="rounded-xl bg-white shadow-lg p-5">
                 <h2 className="text-gray-500 text-lg">Projects ({projects.length})</h2>
             </div>
