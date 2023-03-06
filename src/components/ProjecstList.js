a1fa21ad7128964e14338d8c69363a4d7b24474c
Guillaume Duran <guillaumeduran2@gmail.com>
2023-03-06 21:16:12 +0100

    adds windbnb to projects

--- src/components/ProjecstList.js
+++ src/components/ProjecstList.js
@@ -5,15 +5,24 @@ const ProjectsList = () => {
     const projects = [
         {
             id: 1,
+            img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FwindbnbThumbnail.png%3Falt%3Dmedia%26token%3D183d7d36-d216-4d30-9732-aa6e4ab0e8a5&w=750&q=75',
+            tags: '#React #TailwindCSS',
+            title: 'Windbnb',
+            desc: 'I love airbnb UI, it is so modern and clean. In this project, I built a similar UI to airbnb - windbnb.',
+            demoLink: 'https://main--magnificent-daffodil-30e52e.netlify.app/',
+            codeLink: 'https://github.com/guizput/windbnb'
+        },
+        {
+            id: 2,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FEdieHomageThumbnail.png%3Falt%3Dmedia%26token%3D72c573b5-6389-425c-b947-de63f0f5b2ef&w=640&q=75',
-            tags: '#HTML #CSS #responsive',
+            tags: '#React #TailwindCSS',
             title: 'Edie Home Page',
             desc: 'Made with ParcelJS, SASS and a bit of Javascript for the menu',
             demoLink: 'https://main--wonderful-kulfi-a14ed7.netlify.app/',
             codeLink: 'https://github.com/guizput/edie-homepage'
         },
         {
-            id: 2,
+            id: 3,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FCheckoutThumbnail.png%3Falt%3Dmedia%26token%3Dc7ffdbe3-7206-44f2-b1e6-a6b99bf81901&w=640&q=75',
             tags: '#HTML #CSS #responsive',
             title: 'Checkout page',
@@ -22,7 +31,7 @@ const ProjectsList = () => {
             codeLink: 'https://github.com/guizput/checkout-page'
         },
         {
-            id: 3,
+            id: 4,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FGalleryThumbnail.png%3Falt%3Dmedia%26token%3D92894792-41d1-4d99-8cbb-e828322c87fd&w=640&q=75',
             tags: '#HTML #CSS #responsive',
             title: 'Gallery',
@@ -31,7 +40,7 @@ const ProjectsList = () => {
             codeLink: 'https://github.com/guizput/my-gallery'
         },
         {
-            id: 4,
+            id: 5,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FrecipeBlogThumbnail.png%3Falt%3Dmedia%26token%3D2d696d3c-a8cb-4c7c-907b-561ae1144cc9&w=640&q=75',
             tags: '#HTML #CSS #responsive',
             title: 'Recipe page',
@@ -40,7 +49,7 @@ const ProjectsList = () => {
             codeLink: 'https://github.com/guizput/recipe-page'
         },
         {
-            id: 5,
+            id: 6,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FinteriorConsultantThumbnail.png%3Falt%3Dmedia%26token%3Dfb5f8229-8eb9-4d70-b0d4-fbd2829a0cf0&w=640&q=75',
             tags: '#HTML #CSS #responsive',
             title: 'Interior Consultant',
@@ -49,7 +58,7 @@ const ProjectsList = () => {
             codeLink: 'https://github.com/guizput/interior-consultant'
         },
         {
-            id: 6,
+            id: 7,
             img: 'https://devchallenges.io/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fdevchallenges-1234.appspot.com%2Fo%2FchallengesDesigns%252FmyTeamThumbnail.png%3Falt%3Dmedia%26token%3D3dc1bba0-52b9-440d-8a54-fa31ec7c84d8&w=640&q=75',
             tags: '#HTML #CSS #responsive',
             title: 'My Team Page',
